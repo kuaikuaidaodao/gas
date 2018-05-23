afa69863309fa7d1c373cfbd2ac99157c1585276
kuaikuaidaodao <980894994@qq.com>
2018-05-23 13:26:59 +0800

    chushi

---
+++ src/main/java/com/example/gas/entity/StationinfoList.java
@@ -0,0 +1,72 @@
+package com.example.gas.entity;
+
+/**
+ * @author li
+ * @create 2018-05-11 10:54
+ * @desc 用户
+ **/
+public class StationinfoList {
+    private int index;
+    private String name;
+    private String unit_name;
+    private String longitude;
+    private String latitude;
+    private long device_id;
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public long getDevice_id() {
+        return device_id;
+    }
+
+    public void setDevice_id(long device_id) {
+        this.device_id = device_id;
+    }
+
+    public String getUnit_name() {
+        return unit_name;
+    }
+
+    public void setUnit_name(String unit_name) {
+        this.unit_name = unit_name;
+    }
+}
+
+
+
+
+
+
+
