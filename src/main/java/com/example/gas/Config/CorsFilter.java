5db803238d4c34fed9d0593ce6023ae13dd097d2
kuaikuaidaodao <980894994@qq.com>
2018-05-28 16:16:06 +0800

    跨域

--- src/main/java/com/example/gas/Config/CorsFilter.java
+++ src/main/java/com/example/gas/Config/CorsFilter.java
@@ -7,6 +7,7 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import javax.servlet.Filter;
@@ -20,7 +21,8 @@ public class CorsFilter implements Filter{
 
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        HttpServletRequest reqs = (HttpServletRequest) req;
+        response.setHeader("Access-Control-Allow-Origin", reqs.getHeader("Origin"));
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
