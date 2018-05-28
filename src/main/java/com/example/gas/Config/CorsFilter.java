fa622fdd114f2012249afc6219aed6a775cc838e
kuaikuaidaodao <980894994@qq.com>
2018-05-28 20:16:41 +0800

    chushi

--- src/main/java/com/example/gas/Config/CorsFilter.java
+++ src/main/java/com/example/gas/Config/CorsFilter.java
@@ -9,6 +9,8 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+
 import org.springframework.stereotype.Component;
 import javax.servlet.Filter;
 
@@ -22,16 +24,16 @@ public class CorsFilter implements Filter{
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest reqs = (HttpServletRequest) req;
-        response.setHeader("Access-Control-Allow-Origin", reqs.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
-//        System.out.println("*********************************过滤器被使用**************************");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        System.out.println("*********************************过滤器被使用**************************");
         chain.doFilter(req, res);
     }
     public void init(FilterConfig filterConfig) {}
     public void destroy() {}
-
 
 }
